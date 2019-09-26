41e48b81f0390d25c67929bd5aa9c5ddb5f3dd26
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-09-26 16:56:59 -0300

    Props p1 and p2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
 import React, { Component } from 'react'
 
-const BemVindo = (props) => {
+// const BemVindo = (props) => {
+//     return(
+//         <div id='header'>
+//             <h2>Bem-vindo(a) {props.nome}</h2>
+//             <h3>Tenho {props.idade} anos</h3>
+//         </div>
+//     )
+// }
+const Equipe = (props) => {
     return(
-        <div id='header'>
-            <h2>Bem-vindo(a) {props.nome}</h2>
-            <h3>Tenho {props.idade} anos</h3>
+        <div>
+            <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} url={props.url} />
+            <Social url={props.url}></Social>
+        </div>
+    );
+}
+
+const Sobre = (props) => {
+    return(
+        <div className="sobre">
+            <h2>Olá sou o(a) {props.nome}</h2>
+            <p>
+                <strong>Cargo:</strong> {props.cargo}<br/>
+                <strong>Idade:</strong> {props.idade}
+            </p>
+        </div>
+    );
+}
+
+const Social = (props) => {
+    return(
+        <div className="social">
+            <ul>
+                <li><a href={props.url}>Facebook</a></li>
+                <li><a href={props.url}>Twitter</a></li>
+                <li><a href={props.url}>Linkedin</a></li>
+            </ul>
         </div>
     )
 }
+
 export default class App extends Component {
     render() {
         return (
             <div id='app'>
-                <h1>React is awesome! 2</h1>
-                <BemVindo nome="Pedro" idade="19"/>
-                <BemVindo nome="Guilherme" idade="24"/>
+                <h1>Conheça nossa equipe</h1>
+                <Equipe nome='Lucas' cargo='Programador' idade='25' url='https://www.google.com' />
             </div>
         )
     }
